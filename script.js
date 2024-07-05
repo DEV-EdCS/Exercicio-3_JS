9513d4b53acf776396ded04da00730453b95992f
Sergio <sergio.htp7@gmail.com>
2024-07-05 15:00:46 -0300

    Exercicio 3 Lampada

---
+++ script.js
@@ -0,0 +1,11 @@
+//Questão 01
+const centrais = document.getElementsByClassName('centralizar')
+let nome = prompt("Qual o seu nome?")
+
+const nome_place = document.createElement('h1')
+nome_place.innerHTML = nome
+document.querySelector('body').appendChild(nome_place)
+
+
+
+//Questão 02
